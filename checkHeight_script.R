4a91fb7b4ef71e74e2e2e18c80424b7f8f49b824
apophise <zbzcsn@qq.com>
2017-11-02 00:18:15 +0800

    abcdefg
    sdfsf
    
    sdfsfsdfsdfsdf

--- checkHeight_script.R
+++ checkHeight_script.R
@@ -5,7 +5,8 @@
 
 library(dplyr)
 
-
+#sdfsdsdfsdfs
+sfdsdfs
 age = c(19, 22, 21, 23, 22, 20, 28, 25)
 weight = c(50, 75, 80, 56, 75, 58, 65, 82)
 height = c(1.66, 1.78, 1.90, 1.72, 1.83, 1.68, 1.70, 1.85)
